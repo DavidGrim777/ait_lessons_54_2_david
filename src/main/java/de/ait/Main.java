1f56e917824c763a89de189aef8ceedd3a594658
David Grimberg <davidgrim676@gmail.com>
2024-12-12 20:26:35 +0100

    initial commit

---
+++ src/main/java/de/ait/Main.java
@@ -0,0 +1,13 @@
+package de.ait;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+
+    public static String getGreeting (String name) {
+        return "Hallo, " + "!";
+    }
+}
+
